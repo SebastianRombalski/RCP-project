fdd9b9ae8ffb8e9201146319405e161d1043664e
wirusowy <wirusowy@users.noreply.github.com>
2024-01-12 17:51:12 +0100

    Initial commit

---
+++ src/main/java/com/example/rcpproject/RcpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rcpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcpProjectApplication.class, args);
+    }
+
+}
