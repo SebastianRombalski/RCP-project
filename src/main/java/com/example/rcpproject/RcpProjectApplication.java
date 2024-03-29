796fd1617807f77790cc32cd952f0eae083a94fc
Sebastian Rombalski <sebastian.rombalski@tlen.pl>
2024-03-29 20:26:20 +0100

    123

--- src/main/java/com/example/rcpproject/RcpProjectApplication.java
+++ src/main/java/com/example/rcpproject/RcpProjectApplication.java
@@ -3,13 +3,15 @@ package com.example.rcpproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
 public class RcpProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RcpProjectApplication.class, args);
+//        String haslo ="{argon2}" + Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8().encode("pass");
+//        System.out.println(haslo);
     }
-
 
 }
